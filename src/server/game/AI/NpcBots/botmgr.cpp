507b08120c94aa56637abb596eef271ecdd958c9
ikkj <78132970+ikkj@users.noreply.github.com>
2024-06-13 13:26:38 +0800

    Revert "NPCBots: Fix bots being able to stay in combat when removed via logout"
    This reverts commit 6fc305b1815705ca6a771f995fa04ca471c18e67.

--- src/server/game/AI/NpcBots/botmgr.cpp
+++ src/server/game/AI/NpcBots/botmgr.cpp
@@ -1517,7 +1517,6 @@ void BotMgr::CleanupsBeforeBotDelete(Creature* bot)
     bot->GetBotAI()->UnsummonAll();
     bot->AttackStop();
     bot->CombatStopWithPets(true);
-    bot->getHostileRefMgr().deleteReferences();
 
     //bot->SetOwnerGUID(ObjectGuid::Empty);
     //_owner->m_Controlled.erase(bot);
