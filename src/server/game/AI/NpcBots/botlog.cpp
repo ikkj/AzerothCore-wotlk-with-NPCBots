3f21e42119f55f21f9028d035cdc90922dc6aaa8
ikkj <78132970+ikkj@users.noreply.github.com>
2024-06-13 13:15:18 +0800

    Revert "NPCBots: Add `NpcBot.LogToDB` config option to enable / disable DB logging"
    This reverts commit b095d322ee6e93af4427192fdbfbdca6e2c44527.

--- src/server/game/AI/NpcBots/botlog.cpp
+++ src/server/game/AI/NpcBots/botlog.cpp
@@ -1,6 +1,5 @@
 #include "bot_ai.h"
 #include "botdatamgr.h"
-#include "botmgr.h"
 #include "botlog.h"
 #include "Creature.h"
 #include "DatabaseEnvFwd.h"
@@ -48,9 +47,6 @@ template<typename... Args>
 requires NPCBots::LoggableArguments<Args...>
 void BotLogger::Log(uint16 log_type, Creature const* bot, Args&&... params)
 {
-    if (!BotMgr::IsNpcBotLogEnabled())
-        return;
-
     BotLogImpl(log_type, bot, int32(bot->GetBotAI() ? bot->GetBotAI()->GetBotOwnerGuid() : -1), std::forward<Args>(params)...);
 }
 
@@ -58,9 +54,6 @@ template<typename... Args>
 requires NPCBots::LoggableArguments<Args...>
 void BotLogger::Log(uint16 log_type, uint32 entry, Args&&... params)
 {
-    if (!BotMgr::IsNpcBotLogEnabled())
-        return;
-
     if (Creature const* bot = BotDataMgr::FindBot(entry))
         BotLogger::Log(log_type, bot, std::forward<Args>(params)...);
     else
