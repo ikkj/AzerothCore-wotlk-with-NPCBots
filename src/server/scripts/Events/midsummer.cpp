c91eea2850b8ded880319d016ad9b93429c25870
ikkj <78132970+ikkj@users.noreply.github.com>
2024-07-19 21:30:34 +0800

    修复2

--- src/server/scripts/Events/midsummer.cpp
+++ src/server/scripts/Events/midsummer.cpp
@@ -242,6 +242,7 @@ struct npc_midsummer_bonfire : public ScriptedAI
 
                 if (_spellFocus)
                 {
+                    _spellFocus->DespawnOrUnsummon();
                     _spellFocus->AddObjectToRemoveList();
                     _spellFocus = nullptr;
                 }
@@ -379,6 +380,7 @@ struct npc_midsummer_bonfire_despawner : public ScriptedAI
             // spawnID is 0 for temp spawns
             if (0 == (*itr)->GetSpawnId())
             {
+                (*itr)->DespawnOrUnsummon();
                 (*itr)->AddObjectToRemoveList();
             }
         }
